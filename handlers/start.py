f47ada9cc3b5908c9f345ec8be647a07b10ceba0
xD_Shashank <78837537+theshashankk@users.noreply.github.com>
2021-05-22 11:49:38 +0530

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -27,7 +27,7 @@ from config import BOT_NAME as bn
 @Client.on_message(filters.command("start") & filters.private & ~filters.channel)
 async def start(_, message: Message):
     await message.reply_text(
-        f"""➼ Helloowww 👋 {message.from_user.first_name}! I can play music in voice chats of Telegeam Groups. I have a lot of cool feature that will amaze you!\n\n➼ Do you want me to play music in your Telegram groups'voice chats? Please click the " Command " button below to know how you can use me.\n\n➼ Use the buttons below to know more about me ❤️🔥\n\n➼ Contact my owner [🔥𝕐𝖔𝖚𝖗 - 𝕯𝖆𝖉𝖉𝕪🔥](https://t.me/FallenAngel_xD)\n\nA project by @FallenAngel_xD""",
+        f"""➼ Helloowww 👋 {message.from_user.first_name}! I can play music in voice chats of Telegeam Groups. I have a lot of cool feature that will amaze you!\n\n➼ Do you want me to play music in your Telegram groups'voice chats? Please click the " Command " button below to know how you can use me.\n\n➼ Use the buttons below to know more about me ❤️🔥 """,
         reply_markup=InlineKeyboardMarkup(
             [ 
                 [
@@ -35,11 +35,11 @@ async def start(_, message: Message):
                         "📜 Commands 📜", url="https://telegra.ph/%F0%9D%95%90%F0%9D%96%94%F0%9D%96%9A%F0%9D%96%97---%F0%9D%95%AF%F0%9D%96%86%F0%9D%96%89%F0%9D%96%89%F0%9D%95%AA-%EA%97%84-04-23-5")
                   ],[
                     InlineKeyboardButton(
-                        "❤️ Abour me ❤️", url="https://t.me/aboutoxy"
+                        "🚑 Support group 🚑", url="https://t.me/IDK_LOL3"
                     )
                 ],[ 
                     InlineKeyboardButton(
-                        "🔥 Official Group 🔥", url="https://t.me/X_F0RCE_TEAM"
+                        "Add Me To Ur Group", url="https://t.me/{BOT_USERNAME}?groupstart=true"
                     )]
             ]
         ),
@@ -53,7 +53,7 @@ async def gstart(_, message: Message):
             [
                 [
                     InlineKeyboardButton(
-                        "🔥 My Owner 🔥", url="https://t.me/FallenAngel_xD")
+                        "Support group", url="https://t.me/IDK_LOL3")
                 ]
             ]
         )
